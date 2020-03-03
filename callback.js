22a7bb33e9cc8147e2dfa42afba440c847e2a365
Jonathan.ybarra <jonathan.ybarra@gmail.com>
2020-03-03 20:38:31 -0300

    cll

--- callback.js
+++ callback.js
@@ -1,7 +1,20 @@
 function siguiente(x, done) {
   done(x + 1)
 }
+function doble(x, done) {
+  done(x * 2);
+}
+function calcular(resultado){
+  doble(resultado, res => console.log(`Siguiente del doble => ${res}`))
+}
 
-siguiente(2, resultado => { 
-  console.log('Siguiente de 8 =>', resultado)
-})
+// siguiente(2, resultado => {
+//   console.log('Siguiente de 2 =>', resultado)
+// })
+// doble(2, resultado => console.log('El doble de 2 =>', resultado));
+
+// doble(5,resultado => {
+//   siguiente(resultado , res => console.log('El doble del siguiente',res))
+// })
+
+siguiente(2, calcular )
--- index.js
+++ index.js
@@ -15,3 +15,4 @@ console.log('Doble de 8 =>', doble(8))
 console.log('Siguiente del doble de 8 =>', siguiente(doble(8)))
 console.log('Doble del siguiente de 8 =>', dobleDelSiguiente(8))
 console.log('Cuadruple de 8 =>', doble(doble(8)))
+
